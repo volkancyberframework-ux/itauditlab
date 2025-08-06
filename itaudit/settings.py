6da312b3beffe6d34d8e0dd118ee9d4ddb5426a1
volkanguler <volkanguler@MacBook-Pro-van-volkanguler.local>
2025-08-06 10:15:59 +0200

    Use Cloudinary for media files

--- itaudit/settings.py
+++ itaudit/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 import dj_database_url
 from decouple import config
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 
 AUTH_USER_MODEL = 'core.CustomUser'
