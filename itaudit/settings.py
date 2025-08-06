faea4d8010ce11af63dc0b44d85caaff73c27058
volkanguler <volkanguler@MacBook-Pro-van-volkanguler.local>
2025-08-06 08:55:26 +0200

    Prepare project for Render deployment using .env

--- itaudit/settings.py
+++ itaudit/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['itauditlab.onrender.com']
 
 
 
