4df599929f66dcf8537db2ce9d8948e00ae98f01
volkanguler <volkanguler@MacBook-Pro-van-volkanguler.local>
2025-08-06 09:11:27 +0200

    Add STATIC_URL for static files

--- itaudit/settings.py
+++ itaudit/settings.py
@@ -128,6 +128,9 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_URL = '/static/'
+
+
 # Where collected static files will be stored (Render will serve from here)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
