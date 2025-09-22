ac9b771b773c2c2e456e73180a968f91b61b2db1
volkanguler <volkanguler@MacBook-Pro-van-volkanguler.local>
2025-09-22 20:22:35 +0200

    Add TestQuestion/TestOption; fix 3.9 type hints

--- core/models.py
+++ core/models.py
@@ -1,6 +1,8 @@
+from __future__ import annotations
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+
 
 
 class Course(models.Model):
